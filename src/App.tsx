7403a5b9080247bcef34565997dfb9f062477ee3
Jeffrey Leung <jeffrey24493@gmail.com>
2022-08-16 23:05:46 +0100

    react props + ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ToDos from "./components/ToDos";
+import { TodoProps } from "./components/ToDos";
+
+const arr = [new TodoProps("001"), new TodoProps("002"), new TodoProps("003")];
 
 function App() {
   return (
     <div className="App">
-      <ToDos />
+      <ToDos items={arr} />
     </div>
   );
 }
--- src/components/ToDos.tsx
+++ src/components/ToDos.tsx
@@ -1,12 +1,24 @@
-function ToDos() {
+import React from "react";
+import TodoItem from "./ToDoItem";
+
+export class TodoProps {
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.text = todoText;
+    this.id = new Date().toISOString();
+  }
+}
+
+const ToDos: React.FC<{ items: TodoProps[] }> = (props) => {
   return (
     <ul>
-      <li>123</li>
-      <li>123</li>
-      <li>123</li>
-      <li>123</li>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
     </ul>
   );
-}
+};
 
 export default ToDos;
